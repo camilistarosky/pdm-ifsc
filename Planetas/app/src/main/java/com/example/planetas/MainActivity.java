52538a9450ad2ef0f307df6b4b4ec7c052d0fc30
Camili Starosky <camili.naara@gmail.com>
2024-10-14 11:59:33 -0300

    exemplo bd

--- Planetas/app/src/main/java/com/example/planetas/MainActivity.java
+++ Planetas/app/src/main/java/com/example/planetas/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.planetas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
@@ -10,11 +11,17 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //SQLiteDatabase db;
     ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //db=openOrCreateDatabase("banco", MODE_PRIVATE, null);
+        //db.excecSQL("CREATE table pesoas (id int, nome varchar);");
+
         listView = findViewById(R.id.listView);
         DAOPlaneta dao = new DAOPlaneta();
         PlanetaAdapter planteaAdapter = new PlanetaAdapter(this, R.layout.item_planeta, dao.getPlanetas());
